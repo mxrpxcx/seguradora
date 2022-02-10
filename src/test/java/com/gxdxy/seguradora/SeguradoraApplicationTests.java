7be58d661f6e04b509ca3280409e10da214fa21a
Godoy <renanfgfg@gmail.com>
2022-02-10 11:01:09 -0300

    primeiro commit

---
+++ src/test/java/com/gxdxy/seguradora/SeguradoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxdxy.seguradora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguradoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
